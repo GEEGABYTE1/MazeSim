d193bfba97cff8576ede2eb61963b51b6e9367f2
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-09-08 11:29:39 -0400

    Initial Run through and intro

--- script.py
+++ script.py
@@ -1,1 +1,46 @@
-# Empty 
+from bfs import bfs 
+import time
+
+class Search:
+
+    def __init__(self):
+        self.intro()
+        playing = True 
+        while playing:
+            print('\n')
+            prompt = str(input('Please input a letter or node you would like to start the search from: '))
+            prompt = prompt.strip(" ")
+            prompt = prompt.lower()
+            check = bfs(prompt, '*')
+            if check == None:
+                time.sleep(0.4)
+                print("That letter does not seem valid")
+                time.sleep(0.4)
+                print("Please type a letter from 'a' - 'h'")
+            else:
+                time.sleep(0.4)
+                print("Your letter is valid...")
+                time.sleep(0.2)
+                print("Starting the Search Simulation now...")
+                
+
+
+    def intro(self):
+        print('-'*50)
+        print('-'*50)
+        print('-'*50 + '\n')
+        print('\n')
+        print("------- Find '*' ! An A-Star Simulation -------")
+        print('\n')
+        print('-'*50)
+        print('-'*50)
+        print('-'*50)
+
+
+
+
+
+
+search = Search()
+
+print(search)
