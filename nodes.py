38cc1ab1f3505e19d19c77050520427ef21b8bd2
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-09-08 11:08:31 -0400

    Connected Nodes"

--- nodes.py
+++ nodes.py
@@ -1,21 +1,21 @@
 from graph import*
 from vertex import Vertex
 
-graph = Graph()
+graph = Graph(True)
 
 objects = []
 
-vertex_one = Vertex('/', 1, 5)
-vertex_two = Vertex('/', 1, 10)
-vertex_three = Vertex('/', 1, 15)
+vertex_one = Vertex('a', 1, 5)
+vertex_two = Vertex('b', 1, 10)
+vertex_three = Vertex('c', 1, 15)
 
-vertex_four = Vertex('/', 2, 6)
-vertex_five = Vertex('/', 2, 12)
+vertex_four = Vertex('d', 2, 6)
+vertex_five = Vertex('e', 2, 12)
 vertex_six = Vertex('*', 2, 18)
 
-vertex_seven = Vertex('/', 3, 7)
-vertex_eight = Vertex('/', 3, 14)
-vertex_nine = Vertex('/', 3, 21)
+vertex_seven = Vertex('f', 3, 7)
+vertex_eight = Vertex('g', 3, 14)
+vertex_nine = Vertex('h', 3, 21)
 
 objects.append(vertex_one)
 objects.append(vertex_two)
@@ -27,4 +27,25 @@ objects.append(vertex_seven)
 objects.append(vertex_eight)
 objects.append(vertex_nine)
 
-print(objects)
+graph.add_vertex(vertex_one)            
+graph.add_vertex(vertex_two)
+graph.add_vertex(vertex_three)
+graph.add_vertex(vertex_four)
+graph.add_vertex(vertex_five)
+graph.add_vertex(vertex_six)
+graph.add_vertex(vertex_seven)
+graph.add_vertex(vertex_eight)
+graph.add_vertex(vertex_nine)
+
+ref_num = 0
+while ref_num != len(objects):
+    for vertex in range(len(objects)):
+        cur_vertex = objects[ref_num]
+        if vertex == ref_num:
+            continue 
+        else:
+            graph.add_edge(cur_vertex, objects[vertex])
+    
+    ref_num += 1
+
+
