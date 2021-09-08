1b5089d9c250c84bb262b65d454e5de5cfada58f
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-09-07 22:02:49 -0400

    Vertex File

--- vertex.py
+++ vertex.py
@@ -1,1 +1,11 @@
-# Empty
+class Vertex:
+    def __init__(self, value, x, y):
+        self.value = value 
+        self.edges = {}
+        self.position = (x, y)
+
+    def add_edge(self, vertex, weight=0):
+        self.edges[vertex] = weight
+
+    def get_edges(self):
+        return list(self.edges.keys())
